7303a42899e9725dc5957347eaa219a7792adc52
Vishal-Dolasia <vishaldolasiawork@gmail.com>
2026-01-24 10:57:28 +0530

    Create operations.js

---
+++ lectures/operations.js
@@ -0,0 +1,38 @@
+let value = 3;
+let negval = - value;
+// console.log(negval);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+
+// ********string operations**********
+
+
+
+// console.log("1"+2);   // 12
+// console.log(1+"2");   // 12
+// console.log("1"+2+2); // 122
+// console.log(1+2+"2"); // 32
+
+
+
+
+// console.log(+true); // 1
+// console.log(true+); // error
+
+
+
+/***** PREFIX & SUFFIX ******/
+// count++
+// ++count
+
+
+
