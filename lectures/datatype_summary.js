9b0a53e42ff0c8dc2508d862e050955450e970e8
Vishal-Dolasia <vishaldolasiawork@gmail.com>
2026-01-24 11:28:55 +0530

    Create datatype_summary.js

---
+++ lectures/datatype_summary.js
@@ -0,0 +1,67 @@
+/* 
+    Primitive (call by value
+    
+    ----> copy)
+
+    7 types : String , Number , Boolearn ,null,undefined, Symbol, BigInt
+
+
+
+    pass by Reference(same) (Non-primitive)
+    Array,Objects, Functions
+
+
+
+    JS --> dynamically typed(checked at runtime)
+*/
+
+
+
+/* TYPEOF */
+// let nnumber = 1
+// let string = "1"
+// let bignumber = 1n
+// let a = Symbol(1)
+// let nulll = null
+// let b = undefined
+// let boolll = true
+
+
+
+// console.log(typeof(nnumber));
+// console.log(typeof(string));
+// console.log(typeof(bignumber));
+// console.log(typeof(a));
+// console.log(typeof(nulll)); //object
+// console.log(typeof(b));
+// console.log(typeof(boolll));
+
+
+
+
+
+
+
+
+
+
+//*****************************/
+
+// let arrrr = [1,2,3,"$"]
+// let myobj = {
+//     a : "s",
+//     b : "b",
+//     c : 12,
+//     f : "ggf"
+// }
+
+// let aaaaa = function(aaa){
+//     console.log(aaa+aaa);
+// };
+
+// console.log(typeof(arrrr));
+// console.log(typeof(myobj));
+// console.log(typeof(aaaaa));
+
+
+
