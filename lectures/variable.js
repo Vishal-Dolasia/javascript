80dfbfab4d22b80b6a959519869fb9f454e5214b
Vishal-Dolasia <vishaldolasiawork@gmail.com>
2026-01-24 10:11:43 +0530

    Create variable.js

---
+++ lectures/variable.js
@@ -0,0 +1,25 @@
+const accountId = 22333
+let accountEmail = "vishal@google.com"
+var accountPassword = "123"
+accountCity = "Jaipur"
+let accountState;
+
+
+/*
+const ---> constant // cant be changed once declared
+
+var---> was used earlier but due to scope issues , is not being used
+
+
+let ---> removes all the problem caused by var 
+, dont have any scope issue
+
+
+accountCity = "Jaipur"  --> not a good practice but this is also a good way to declare a variable
+
+
+let accountState; --> undefined
+
+*/
+
+
