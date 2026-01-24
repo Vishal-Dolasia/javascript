c4b06cee297e4b2e57701e09d7b42e34e5b4e39e
Vishal-Dolasia <vishaldolasiawork@gmail.com>
2026-01-24 12:17:30 +0530

    Create string.js

---
+++ lectures/string.js
@@ -0,0 +1,23 @@
+let count = 12
+let name = "vishal"
+
+console.log(`my name is ${name} and my repo count is ${count}`);
+
+
+
+
+let naa = new String("vihsla")
+console.log(naa[0]);
+console.log(typeof naa[0]);
+
+
+/*
+replace
+includes
+slice
+trim
+charAt
+indexOf
+substring
+split
+*/
