dc734affa2e0c2fff3ee2ea96480f2533cabca5f
Vishal-Dolasia <vishaldolasiawork@gmail.com>
2026-01-24 10:57:01 +0530

    Create comparison.js

---
+++ lectures/comparison.js
@@ -0,0 +1,25 @@
+
+console.log(2 > 2);
+console.log(2 >= 2);
+console.log(2 < 2);
+console.log(2 == 2);
+console.log(2 != 2);
+
+
+
+
+/***** comparison ******/
+
+// console.log(2 > 2);
+// console.log(2 >= 2);
+// console.log(2 < 2);
+// console.log(2 == 2);
+// console.log(2 != 2);
+
+/* 
+----> NUMBER AND NUMBER COMPARISON FINE
+----> DIFFERENT DATATYPE (PROBLEM)
+
+----> use strict check using  ( " === " )
+
+*/
