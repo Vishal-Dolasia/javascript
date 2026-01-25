4c5568a599a7a6495535c7a045139f05256b4f19
Vishal-Dolasia <vishaldolasiawork@gmail.com>
2026-01-25 10:04:50 +0530

    Create num_and_math.js

---
+++ lectures/num_and_math.js
@@ -0,0 +1,41 @@
+/*
+toFixed ---> for decimal
+
+toFixed(1) ---> for 1 decimal point
+12.2
+
+toFixed(2) ---> for 2 decimal point
+12.22
+*/
+
+
+
+/*
+toPrecision(4)
+1234.7 -- > 1235
+*/
+
+
+
+/************* MATHS ****************** */
+/*
+floor
+ceil
+abs
+round
+min(4,5,6,7)
+max(4,5,6,7)
+
+
+
+Math.random() -->  between 0 and 1
+
+(Math.random()*10)+1 --> between 1 and 10
+
+
+
+(Math.random()*(max-min+1))+min --> between min and max
+
+
+
+*/
